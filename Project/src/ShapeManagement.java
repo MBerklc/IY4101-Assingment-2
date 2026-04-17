70ae06cce12c6a009f49bafaf4c6053c1fc0fc4f
MBerklc <klcmberk@gmail.com>
2026-04-17 22:45:12 +0100

    ShapeManagement class improvments

--- Project/src/ShapeManagement.java
+++ Project/src/ShapeManagement.java
@@ -78,61 +78,81 @@ public class ShapeManagement {
 
             switch (shapeChoice){
                 case "C":
-                    System.out.print("Enter X coordinate: ");
-                    x = cin.nextInt();
-                    System.out.print("Enter Y coordinate: ");
-                    y = cin.nextInt();
-                    System.out.print("Enter radius: ");
-                    double radius = cin.nextDouble();
-                    shapeList.addShape(new Circle(new Coordinates(x, y), radius));
-                    System.out.println("Circle added! " + line1);
+                    try {
+                        System.out.print("Enter X coordinate: ");
+                        x = cin.nextInt();
+                        System.out.print("Enter Y coordinate: ");
+                        y = cin.nextInt();
+                        System.out.print("Enter radius: ");
+                        double radius = cin.nextDouble();
+                        shapeList.addShape(new Circle(new Coordinates(x, y), radius));
+                        System.out.println("Circle added! " + line1);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input! Try again.");
+                        cin.nextLine(); // clears buffer better
+                    }
                     break;
                 case "S":
-                    System.out.print("Enter X coordinate: ");
-                    x = cin.nextInt();
-                    System.out.print("Enter Y coordinate: ");
-                    y = cin.nextInt();
-                    System.out.print("Enter side: ");
-                    double side = cin.nextDouble();
-                    shapeList.addShape(new Square(new Coordinates(x, y), side));
-                    System.out.println("Square added! " + line1);
+                    try {
+                        System.out.print("Enter X coordinate: ");
+                        x = cin.nextInt();
+                        System.out.print("Enter Y coordinate: ");
+                        y = cin.nextInt();
+                        System.out.print("Enter side: ");
+                        double side = cin.nextDouble();
+                        shapeList.addShape(new Square(new Coordinates(x, y), side));
+                        System.out.println("Square added! " + line1);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input! Try again.");
+                        cin.nextLine(); // clears buffer better
+                    }
                     break;
                 case "R":
-                    System.out.println(line1);
-                    System.out.print("Enter X coordinate: ");
-                    x = cin.nextInt();
-                    System.out.print("Enter Y coordinate: ");
-                    y = cin.nextInt();
-                    System.out.print("Enter length: ");
-                    double length = cin.nextDouble();
-                    System.out.print("Enter width: ");
-                    double width = cin.nextDouble();
-                    shapeList.addShape(new Rectangle(new Coordinates(x, y), length, width));
-                    System.out.println("Rectangle added! " + line1);
+                    try {
+                        System.out.println(line1);
+                        System.out.print("Enter X coordinate: ");
+                        x = cin.nextInt();
+                        System.out.print("Enter Y coordinate: ");
+                        y = cin.nextInt();
+                        System.out.print("Enter length: ");
+                        double length = cin.nextDouble();
+                        System.out.print("Enter width: ");
+                        double width = cin.nextDouble();
+                        shapeList.addShape(new Rectangle(new Coordinates(x, y), length, width));
+                        System.out.println("Rectangle added! " + line1);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input! Try again.");
+                        cin.nextLine(); // clears buffer better
+                    }
                     break;
                 case "T":
-                    System.out.print("Enter vertexA's X coordinate: ");
-                    int ax = cin.nextInt();
-                    System.out.print("Enter vertexA's Y coordinate: ");
-                    int ay = cin.nextInt();
-                    Coordinates v1 = new Coordinates(ax, ay);
-                    System.out.print("Enter vertexB's X coordinate: ");
-                    int bx = cin.nextInt();
-                    System.out.print("Enter vertexB's Y coordinate: ");
-                    int by = cin.nextInt();
-                    Coordinates v2 = new Coordinates(bx, by);
-                    System.out.print("Enter vertexC's X coordinate: ");
-                    int cx = cin.nextInt();
-                    System.out.print("Enter vertexC's Y coordinate: ");
-                    int cy = cin.nextInt();
-                    Coordinates v3 = new Coordinates(cx, cy);
-                    Triangle t1 = new Triangle(v1, v2, v3);
+                    try {
+                        System.out.print("Enter vertexA's X coordinate: ");
+                        int ax = cin.nextInt();
+                        System.out.print("Enter vertexA's Y coordinate: ");
+                        int ay = cin.nextInt();
+                        Coordinates v1 = new Coordinates(ax, ay);
+                        System.out.print("Enter vertexB's X coordinate: ");
+                        int bx = cin.nextInt();
+                        System.out.print("Enter vertexB's Y coordinate: ");
+                        int by = cin.nextInt();
+                        Coordinates v2 = new Coordinates(bx, by);
+                        System.out.print("Enter vertexC's X coordinate: ");
+                        int cx = cin.nextInt();
+                        System.out.print("Enter vertexC's Y coordinate: ");
+                        int cy = cin.nextInt();
+                        Coordinates v3 = new Coordinates(cx, cy);
+                        Triangle t1 = new Triangle(v1, v2, v3);
 
-                    //Check if it's a real triangle
-                    if(t1.getArea() != 0){
-                        shapeList.addShape(t1);
-                        System.out.println("Triangle added! " + line1);
-                    } else System.out.print("This is not a triangle, try again...");
+                        //Check if it's a real triangle
+                        if(t1.getArea() != 0){
+                            shapeList.addShape(t1);
+                            System.out.println("Triangle added! " + line1);
+                        } else System.out.print("This is not a triangle, try again...");
+                    } catch (Exception e) {
+                        System.out.println("Invalid input! Try again.");
+                        cin.nextLine(); // clears buffer better
+                    }
                     break;
                 case "E":
                     System.out.printf("Going back to main menu... %s", line1);
